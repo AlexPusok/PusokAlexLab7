50ddc50e3fca643be5bf7c2e9128fd5cb451e73a
PusokAlex <pusokalex12@gmail.com>
2024-11-28 17:33:04 +0200

    Laborator 8

---
+++ Data/ShoppingListDatabase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using PusokAlexLab7.Models;
+
+namespace PusokAlexLab7.Data
+{
+    public class ShoppingListDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+        public ShoppingListDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Shoplist>().Wait();
+        }
+        public Task<List<Shoplist>> GetShopListsAsync()
+        {
+            return _database.Table<Shoplist>().ToListAsync();
+        }
+        public Task<Shoplist> GetShopListAsync(int id)
+        {
+            return _database.Table<Shoplist>()
+            .Where(i => i.ID == id)
+           .FirstOrDefaultAsync();
+        }
+        public Task<int> SaveShopListAsync(Shoplist slist)
+        {
+            if (slist.ID != 0)
+            {
+                return _database.UpdateAsync(slist);
+            }
+            else
+            {
+                return _database.InsertAsync(slist);
+            }
+        }
+        public Task<int> DeleteShopListAsync(Shoplist slist)
+        {
+            return _database.DeleteAsync(slist);
+        }
+
+    }
+}
---
+++ ListEntryPage.xaml.cs
@@ -0,0 +1,34 @@
+using PusokAlexLab7.Models;
+
+namespace PusokAlexLab7;
+
+public partial class ListEntryPage : ContentPage
+{
+	public ListEntryPage()
+	{
+		InitializeComponent();
+	}
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetShopListsAsync();
+    }
+    async void OnShopListAddedClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ListPage
+        {
+            BindingContext = new Shoplist()
+        });
+    }
+    async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = e.SelectedItem as Shoplist
+            });
+        }
+    }
+
+}
